5cf709365c4364b7b610f5ba20fc75deb00b4bbc
IsamuNakamura <isamu_nakamura@in-eng.jp>
2024-05-11 09:19:31 +0900

    fix provider type

--- front/src/app/signin/page.tsx
+++ front/src/app/signin/page.tsx
@@ -1,30 +1,26 @@
 "use client";
 
 import { Button, Container, Group, Text, Title } from "@mantine/core";
-import { getProviders, signIn } from "next-auth/react";
+import type { BuiltInProviderType } from "next-auth/providers/index";
+import {
+	type ClientSafeProvider,
+	type LiteralUnion,
+	getProviders,
+	signIn,
+} from "next-auth/react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
-type Provider = {
-	callbackUrl: string;
-	id: string;
-	name: string;
-	signinUrl: string;
-	type: string;
-};
+type Provider = Record<
+	LiteralUnion<BuiltInProviderType, string>,
+	ClientSafeProvider
+> | null;
 
-export default function LoginPage() {
-	const [providers, setProviders] = useState<Provider[]>([]);
-
-	useEffect(() => {
-		const fetchProviders = async () => {
-			const providersData = await getProviders();
-
-			providersData && setProviders(Object.values(providersData));
-		};
-
-		fetchProviders();
-	}, []);
+export default async function LoginPage() {
+	const providersData: Provider = await getProviders();
+	if (!providersData) {
+		return null;
+	}
+	const providers = Object.values(providersData);
 
 	const handleClickSignIn = (providerId: string) => {
 		signIn(providerId, { callbackUrl: "/" });
